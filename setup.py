55e10c0b094d11886df96699189d58e023060143
Stephen Crosby <stevecrozz@gmail.com>
2014-06-18 22:00:36 -0700

    create a python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='optionshouse-api-client',
+    version='0.1.0',
+    description='python wrapper for the optionshouse trading platform api',
+    author='Stephen Crosby',
+    author_email='stevecrozz@gmail.com',
+    url='https://github.com/stevecrozz/optionshouse-api-client',
+    packages=[
+        'optionshouse'
+    ],
+)
