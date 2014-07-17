236733721f8f7311f2f6ead509ddcb33e792a947
Stephen Crosby <stevecrozz@gmail.com>
2014-07-17 15:56:21 -0700

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='optionshouse',
-    version='0.1.0',
+    version='0.2.0',
     description='python wrapper for the optionshouse trading platform api',
     author='Stephen Crosby',
     author_email='stevecrozz@gmail.com',
